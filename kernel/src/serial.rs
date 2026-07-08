4aad563dbe562aeeff3a02ecd97578b8eae3fe21
Akash Reddy <akashreddy2003@gmail.com>
2026-07-08 20:45:31 +0530

    Replaced call_once with lazylock

--- kernel/src/serial.rs
+++ kernel/src/serial.rs
@@ -1,20 +1,18 @@
 use core::fmt::{Arguments, Write};
-use spin::{Mutex, Once};
+use spin::{LazyLock, Mutex};
 use uart_16550::{Config, Uart16550Tty, backend::PioBackend};
 
-static SERIAL: Once<Mutex<Uart16550Tty<PioBackend>>> = Once::new();
-
-pub fn get_serial() -> &'static Mutex<Uart16550Tty<PioBackend>> {
-    SERIAL.call_once(|| {
+static SERIAL: LazyLock<Mutex<Uart16550Tty<PioBackend>>> = LazyLock::new(|| {
         Mutex::new(unsafe {
             Uart16550Tty::new_port(0x3f8, Config::default())
                 .expect("Serial couldn't be initialized")
         })
-    })
-}
+    }
+);
 
 pub fn _print(arg: Arguments) {
-    let mut uart = get_serial().lock();
-    let _ = uart.write_fmt(arg);
-    drop(uart);
+    x86_64::instructions::interrupts::without_interrupts(|| {
+        let mut uart = SERIAL.lock();
+        uart.write_fmt(arg).expect("Printing to serial failed");
+    });
 }
